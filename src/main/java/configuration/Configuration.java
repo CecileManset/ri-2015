9dca492f98b3f3c03ee029a8302f4963b2275965
Cécile Manset <cecile.manset@gmail.com>
2014-11-23 17:37:04 +0100

    ass qrel files and correct mistakes

--- src/main/java/configuration/Configuration.java
+++ src/main/java/configuration/Configuration.java
@@ -3,7 +3,7 @@ package configuration;
 public class Configuration {
 	
 	public static String PATH_TO_CORPUS = "/Users/cecilemanset/Documents/INSA/5IL/WebSem/CORPUS/D";
-	public static String PATH_TO_QREL = "/Users/cecilemanset/Documents/INSA/5IL/WebSem/qrels/";
+	public static String PATH_TO_QREL = "./qrels/";
 	public static String PATH_TO_REQUESTS = "./requests.txt";
 	public static String PATH_TO_STOPLIST = "/Users/cecilemanset/Documents/INSA/5IL/WebSem/workspace/ri-2015/stopliste.txt";
 
