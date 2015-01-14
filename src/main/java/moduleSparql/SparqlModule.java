4eb3d5ec0b2c17a9a39e186ecc9164d95f52a0b9
Nicolas <nicolas.levaique@ubleam.com>
2015-01-14 11:59:32 +0100

    initiate the sparql module... I think

--- src/main/java/moduleSparql/SparqlModule.java
+++ src/main/java/moduleSparql/SparqlModule.java
@@ -1,6 +1,14 @@
 package moduleSparql;
 
+import java.util.Map;
+
 public class SparqlModule {
+	
+	private SparqlClient sparqlClient;
+	
+	public SparqlModule() {
+		sparqlClient = new SparqlClient("localhost:3030/space");
+	}
 	
 	public String[] extendRequest(String[] request) {
 		String[] extendedRequest = null;
@@ -15,6 +23,21 @@ public class SparqlModule {
         	System.out.println("Server is down");
         }
 		return extendedRequest;
+	}
+	
+	private String[] returnLabels(String[] request) {
+		String query = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
+                + "SELECT ?labels WHERE\n"
+                + "{\n"
+                + "    ?subject rdfs:label ?object.\n"
+                + "}\n"
+                + "GROUP BY ?piece\n";
+        Iterable<Map<String, String>> results = sparqlClient.select(query);
+        System.out.println("nombre de personnes par pièce:");
+        for (Map<String, String> result : results) {
+            System.out.println(result.get("subject") + " : " + result.get("label"));
+            
+        }
 	}
 
 }
